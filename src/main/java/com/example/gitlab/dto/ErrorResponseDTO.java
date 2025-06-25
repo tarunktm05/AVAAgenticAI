30c90fedc3fb896a0d5894047d20b28812c000ca
tarunktm05 <92526263+tarunktm05@users.noreply.github.com>
2025-06-25 13:20:25 +0530

    Add ErrorResponseDTO

---
+++ src/main/java/com/example/gitlab/dto/ErrorResponseDTO.java
@@ -0,0 +1,12 @@
+package com.example.gitlab.dto;
+
+public class ErrorResponseDTO {
+
+    private String error;
+
+    public ErrorResponseDTO(String error) {
+        this.error = error;
+    }
+
+    // Getter and Setter
+}
